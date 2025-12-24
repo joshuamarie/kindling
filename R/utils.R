be2d28d6f7ed88ca51657b77d5882278b1dacbac
unknown <joshstat126@gmail.com>
2025-12-24 11:16:42 +0800

    Extra imports

---
+++ R/utils.R
@@ -0,0 +1,6 @@
+#' @importFrom stats reorder setNames
+#' @importFrom rlang :=
+NULL
+
+# To avoid R CMD check notes about undefined global variables
+utils::globalVariables(c("x_names", "rel_imp", "object", "new_data"))
