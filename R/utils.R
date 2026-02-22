ec6eee018e3e708721960667be16c73f835f9a01
unknown <joshstat126@gmail.com>
2026-02-22 20:12:44 +0800

    `%||%`

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,6 @@ NULL
 
 # To avoid R CMD check notes about undefined global variables
 utils::globalVariables(c("x_names", "rel_imp", "object", "new_data", "nn_module", "fit_class"))
+
+`%||%` = function (x, y) 
+    if (is.null(x)) y else x
