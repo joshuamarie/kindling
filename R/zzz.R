a4f663350e74cd65c5cf6ccf2774af8f30302056
unknown <joshstat126@gmail.com>
2026-07-15 13:22:09 +0800

    For `R CMD check` checks

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,14 @@
 #' @keywords internal
 .onLoad = function(libname, pkgname) {
     vctrs::s3_register("vip::vi_model", "ffnn_fit")
-    make_kindling()
+
+    has_tidymodels_engines =
+        requireNamespace("parsnip", quietly = TRUE) &&
+        utils::packageVersion("parsnip") >= "1.0.0" &&
+        requireNamespace("dials", quietly = TRUE) &&
+        requireNamespace("tune", quietly = TRUE)
+
+    if (has_tidymodels_engines) {
+        make_kindling()
+    }
 }
-
-
